6383977c8145f45118b50d80b91370b906338da1
RoTour <rothewawa@gmail.com>
2021-11-13 14:00:22 +0100

    Bg icosahedron no longer resetting when scrolling

--- src/containers/FullContent/FullContent.tsx
+++ src/containers/FullContent/FullContent.tsx
@@ -1,5 +1,7 @@
-import { FC, useEffect } from 'react';
+import React, { FC, useEffect, useRef } from 'react';
 import { useHistory } from 'react-router-dom';
+import * as THREE from 'three';
+import { Camera, Euler, IcosahedronGeometry, Line, LineBasicMaterial, Scene, Vector3 } from 'three';
 import Contact from '../../pages/Contact/Contact';
 import Home from '../../pages/Home/Home';
 import Projects from '../../pages/Projects/Projects';
@@ -8,15 +10,85 @@ import './FullContent.module.css';
 
 type FullContentProps = {}
 
+
+const setupThree = (posRef: React.MutableRefObject<Vector3 | null | undefined>, rotationRef: React.MutableRefObject<Euler | null | undefined>) => {
+  let scene: Scene | null = new THREE.Scene();
+  let camera: Camera | null = new THREE.PerspectiveCamera(
+    75,
+    window.innerHeight / window.innerHeight,
+    0.1,
+    1000,
+  );
+  const canvas: any = document.querySelector('#bg')!!;
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    alpha: true,
+  });
+  let shouldAnimate = true;
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth * 0.95, window.innerHeight);
+  camera.position.setZ(30);
+  renderer.setClearColor(0xffffff, 0);
+  renderer.render(scene, camera);
+  const geometry = new THREE.IcosahedronGeometry(15, 0);
+  const material = new THREE.LineBasicMaterial({
+    color: 0x44D62C,
+    opacity: 0.15,
+    transparent: true,
+    linewidth: 5,
+  });
+  // const icosahedron = new THREE.Mesh(geometry, material);
+  let icosahedron: Line<IcosahedronGeometry, LineBasicMaterial> | null = new THREE.Line(geometry, material);
+  if (posRef && posRef.current && rotationRef && rotationRef.current) {
+    icosahedron.setRotationFromEuler(rotationRef.current)
+    icosahedron.position.add(posRef.current)
+  } else {
+    icosahedron.position.setX(10);
+  }
+  scene.add(icosahedron);
+
+  const pointLight = new THREE.PointLight(0xffffff);
+  const ambiant = new THREE.AmbientLight(0xffffff);
+  pointLight.position.set(5, 5, 15);
+  scene.add(pointLight, ambiant);
+
+  function animate() {
+    if(shouldAnimate) {
+      requestAnimationFrame(animate);
+      renderer.render(scene!, camera!);
+      icosahedron!.rotation.x += 0.001;
+      icosahedron!.rotation.y += 0.00005;
+      icosahedron!.rotation.z += 0.001;
+    }
+  }
+
+  animate();
+  return () => {
+    if (icosahedron) {
+      posRef.current = icosahedron.position;
+      rotationRef.current = icosahedron.rotation;
+    }
+    shouldAnimate = false;
+    ambiant.dispose();
+    pointLight.dispose();
+    material.dispose();
+    geometry.dispose();
+    renderer.dispose();
+    icosahedron = null;
+    scene = null;
+    camera = null;
+  }
+};
+
 const FullContent: FC<FullContentProps> = () => {
   const navigator = useHistory();
+  const bgObjectPosition: React.MutableRefObject<Vector3 | null | undefined> = useRef();
+  const bgObjectRotation: React.MutableRefObject<Euler | null | undefined> = useRef();
 
   useEffect(() => {
     const callback = (newRoute: string): ((entries: any) => void) => {
       return (entries: any) => {
-        console.log(entries, entries.length, entries[0].intersectionRatio );
         if (navigator.location.hash !== newRoute && entries.length > 0 && entries[0].intersectionRatio >= 0.8) {
-          console.log(`should push ${newRoute}`);
           navigator.push(newRoute);
         }
       };
@@ -36,41 +108,7 @@ const FullContent: FC<FullContentProps> = () => {
         observer.observe(target)
       }
     })
-
-    // let observer = new IntersectionObserver(callback('#robin'), options);
-    // const target = document.querySelector('#robin');
-    // if (target) {
-    //   observer.observe(target)
-    // }
-    // const componentsData: { id: string, top?: number, bottom?: number }[] = [
-    //   { id: 'home' },
-    //   { id: 'robin' },
-    //   { id: 'projects' },
-    //   { id: 'contact' },
-    // ];
-    // componentsData.forEach((it) => {
-    //   const range = document.createRange();
-    //   const node = document.querySelector(`#${it.id}`);
-    //   if (node) {
-    //     range.selectNodeContents(node);
-    //     const rects = range.getClientRects();
-    //     if (rects.length > 0) {
-    //       console.log(rects);
-    //       it.top = rects[0].top;
-    //       it.bottom = rects[0].bottom
-    //     }
-    //   } else {
-    //     console.log(`no node ${it.id}`);
-    //   }
-    // })
-    // const range = document.createRange();
-    // const homeNode = document.querySelector('#home')!;
-    // const robinNode = document.querySelector('#robin')!;
-    // range.selectNodeContents(homeNode);
-    // range.selectNodeContents(robinNode);
-    // const rects = range.getClientRects();
-    // console.log(componentsData);
-    // console.log(componentsData);
+    return setupThree(bgObjectPosition, bgObjectRotation);
   });
   return <>
     <Home id={'home'}/>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,58 +1,11 @@
 import { FC, useEffect, useRef } from 'react';
 import { IoIosArrowDown } from 'react-icons/all';
 import { useHistory } from 'react-router-dom';
-import * as THREE from 'three';
 import css from './Home.module.css';
 
 type HomeProps = {
   id: string,
 }
-
-const setupThree = () => {
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerHeight / window.innerHeight,
-    0.1,
-    1000,
-  );
-  const canvas: any = document.querySelector('#bg')!!;
-  const renderer = new THREE.WebGLRenderer({
-    canvas,
-    alpha: true,
-  });
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(window.innerWidth * 0.95, window.innerHeight);
-  camera.position.setZ(30);
-  renderer.setClearColor(0xffffff, 0);
-  renderer.render(scene, camera);
-  const geometry = new THREE.IcosahedronGeometry(15, 0);
-  const material = new THREE.LineBasicMaterial({
-    color: 0x44D62C,
-    opacity: 0.15,
-    transparent: true,
-    linewidth: 5,
-  });
-  // const icosahedron = new THREE.Mesh(geometry, material);
-  const icosahedron = new THREE.Line(geometry, material);
-  icosahedron.position.setX(10);
-  scene.add(icosahedron);
-
-  const pointLight = new THREE.PointLight(0xffffff);
-  const ambiant = new THREE.AmbientLight(0xffffff);
-  pointLight.position.set(5, 5, 15);
-  scene.add(pointLight, ambiant);
-
-  function animate() {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
-    icosahedron.rotation.x += 0.001;
-    icosahedron.rotation.y += 0.00005;
-    icosahedron.rotation.z += 0.001;
-  }
-
-  animate();
-};
 
 const Home: FC<HomeProps> = (props) => {
   const navigator = useHistory();
@@ -62,10 +15,11 @@ const Home: FC<HomeProps> = (props) => {
 
   useEffect(() => {
     if (firstTime.current) {
-      navigator.push('#home');
+      if (navigator.location.hash === '') {
+        navigator.push('#home');
+      }
       firstTime.current = false
     }
-    setupThree();
     nextRef.current = () => {
       document.querySelector('#robin')
         ?.scrollIntoView({ behavior: 'smooth', block: 'start' });
