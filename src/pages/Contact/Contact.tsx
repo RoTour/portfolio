577bf03b0115acda0c8b2db5a826ee0c6f04c9d0
RoTour <rothewawa@gmail.com>
2021-12-05 14:28:54 +0100

    Contact form

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,9 +1,9 @@
+import axios from 'axios';
 import { FC } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { MdSend } from 'react-icons/all';
 import Title from '../../common/Title/Title';
 import css from './Contact.module.css';
-import * as nodemailer from 'nodemailer'
 
 type ContactProps = {
   id: string
@@ -16,35 +16,33 @@ type Inputs = {
 }
 
 const Contact: FC<ContactProps> = (props) => {
-  const { register, handleSubmit, formState: { errors } } = useForm();
+  const { register, handleSubmit, formState: { errors }, setValue } = useForm();
   const onSubmit: SubmitHandler<Inputs> = (data) => {
-    console.log(data);
-    const transporter = nodemailer.createTransport(
-      {
-        service: process.env.CONTACT_EMAIL_SERVICE ?? 'notfound',
-        auth: {
-          user: process.env.CONTACT_SENDER_EMAIL,
-          pass: process.env.CONTACT_SENDER_PASSWORD,
-        },
-        from: data.email,
-        to: process.env.CONTACT_EMAIL ?? 'notfound',
-        subject: `Contact From portfolio Website ${data.company ? `(${data.company})` : ''}`
-      }
-    )
-    console.log(transporter);
-  }
+    axios({
+      method: 'POST',
+      url: '',
+      data: {
+        'from': data.email,
+        'html': `<p>${data.message}</p>`,
+        'subject': `Message from portfolio website ${data.company ? `(${data.company})` : ''})`,
+      },
+    }).then(() => {
+      setValue('email', '');
+      setValue('company', '');
+      setValue('message', '');
+    });
+  };
 
   return <div className={css.formContainer} id={props.id}>
     <div className={css.small}>
       <Title>Contact</Title>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className={css.formRow}>
-          <label className={css.formLabel}>
-            ✉️ Email
+          <label className={[css.formLabel, `${errors.email ? css.error : ''}`].join(' ')}>
+            ✉️ Email {errors.email && ' - This field is required'}
             <input defaultValue={''} type={'email'} className={css.formInput}
                    placeholder={'ex. : john.doe@gmail.com'}
                    {...register('email', { required: true })} />
-            {errors.email && <span>This field is required</span>}
           </label>
           <label className={css.formLabel}>
             🏢 Company (Optional)
@@ -53,14 +51,13 @@ const Contact: FC<ContactProps> = (props) => {
                    {...register('company')} />
           </label>
         </div>
-        <label className={css.formLabel}>
-          📜 Message
+        <label className={[css.formLabel, `${errors.message ? css.error : ''}`].join(' ')}>
+          📜 Message {errors.message && ' - This field is required '}
           <textarea defaultValue={''} className={css.formInput} rows={4}
                     placeholder={'Your message ...'}
                     {...register('message', { required: true, minLength: 5, maxLength: 5000 })}/>
-          {errors.message && <span>This field is required</span>}
         </label>
-        <button type={'submit'} className={css.submitBtn}>Send Message <MdSend size={'1.1em'}/> </button>
+        <button type={'submit'} className={css.submitBtn}>Send <MdSend size={'1.1em'}/></button>
       </form>
     </div>
   </div>;
